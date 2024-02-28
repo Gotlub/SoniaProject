1cd23e3b559dd5ef7c0b6c6fcbbd8174e6405435
Nicolas Frere <gotlub@gmail.com>
2024-02-28 09:56:00 +0100

    formulaire de connexion

--- src/Form/UserPasswordType.php
+++ src/Form/UserPasswordType.php
@@ -17,37 +17,37 @@ class UserPasswordType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-        ->add('plainPassword', RepeatedType::class, [
+        ->add('plainPassword', PasswordType::class, [
+            'attr' => ['class' => 'form-control'],
+            'label' => 'Mot de passe',
+            'label_attr' => ['class' => 'form-label'],
+            'constraints' => [new Assert\NotBlank()]
+        ])
+        ->add('newPassword', RepeatedType::class, [
             'type' => PasswordType::class,
             'first_options' => [
                 'attr' => [
                     'class' => 'form-control'
                 ],
-                'label' => 'Mot de passe',
+                'label' => 'Nouveau mot de passe',
                 'label_attr' => [
-                    'class' => 'form-label  mt-4'
+                    'class' => 'form-label'
                 ]
             ],
             'second_options' => [
                 'attr' => [
                     'class' => 'form-control'
                 ],
-                'label' => 'Confirmation du mot de passe',
+                'label' => 'Confirmation du Nouveau mot de passe',
                 'label_attr' => [
-                    'class' => 'form-label  mt-4'
+                    'class' => 'form-label'
                 ]
             ],
             'invalid_message' => 'Les mots de passe ne correspondent pas.'
         ])
-        ->add('newPassword', PasswordType::class, [
-            'attr' => ['class' => 'form-control'],
-            'label' => 'Nouveau mot de passe',
-            'label_attr' => ['class' => 'form-label mt-4'],
-            'constraints' => [new Assert\NotBlank()]
-        ])
         ->add('submit', SubmitType::class, [
             'attr' => [
-                'class' => 'btn btn-primary mt-4'
+                'class' => 'btn btn-primary'
             ],
             'label' => 'Changer mon mot de passe'
         ]);
